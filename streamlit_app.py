ac6ec1dc3431988f635d89e658d5e5c505fab1cc
helinyl <helillustrates@gmail.com>
2026-06-01 20:03:49 +0300

    Change page icon from lock to key symbol

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Cipherish App", page_icon="🔐", layout="wide")
+st.set_page_config(page_title="Cipherish App", page_icon="⚿", layout="wide")
 
 st.markdown(
     """
